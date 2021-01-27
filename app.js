9955754474bfff9c598df45a45e721b37f036788
Francisco Correa <fcorrea.fernandez@gmail.com>
2021-01-27 07:53:23 -0300

    update style in css

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 //  definir arrays
-let card = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+let card = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 let suite = ['&spades;', '&clubs;', '&hearts;', '&diams;']
 
 // funcion para generar indice string, random es número
 function randomCardNumber(a) {
     return card[a]
 }
+
 function randomSuiteNumber(b) {
     return suite[b]
 }
-// random numero y random suite (número)
-// DOM numero y suite (inyección HTML - funcion - (numero-string) )
+// random card y random suite (número)
+// DOM card y suite (inyección HTML - funcion - (numero->string) )
 // colores '&hearts;', '&diams;' = red
 // rotar el suite_2 a 180 grados
 window.onload = () => {
@@ -26,11 +27,3 @@ window.onload = () => {
     document.getElementById("suite_2").style.transform = "rotate(180deg)";
 
 }
-
-
-// DOM - evento onload - suite - pinta
-// other functions
-//console.log(
-//    Pinta: ${pinta[randomPinta]}
-//    Numero: ${numero[randomPinta]}
-//)
